f923fdd14e682153d527f8aebb376547ba708758
Beat Richartz <beat.richartz@gmail.com>
2015-05-23 19:26:30 +0100

    Added travis.yml and docs generation

--- mix.exs
+++ mix.exs
@@ -5,14 +5,18 @@ defmodule Classifiers.Mixfile do
     [
       app: :fsm,
       version: "0.0.1",
-      elixir: ">= 1.0.0",
+      elixir: "~> 1.0.0 or ~> 1.1-dev",
       deps: deps,
-      package: [
-        contributors: ["Beat Richartz"],
-        licenses: ["MIT"],
-        links: %{"Github": "https://github.com/elixir-cortex/classifiers"}
-      ],
+      package: package, 
       description: "Implementations of classifier algorithms"
+    ]
+  end
+
+  def package do
+    [
+      contributors: ["Beat Richartz"],
+      licenses: ["MIT"],
+      links: %{"Github": "https://github.com/elixir-cortex/classifiers"}
     ]
   end
 
@@ -21,6 +25,20 @@ defmodule Classifiers.Mixfile do
   end
 
   defp deps do
-    [{:csv, "~> 0.2.0"}]
+    [
+      {:csv, "~> 0.2.0", only: :test},
+      {:ex_doc, "~> 0.7.1", only: :docs},
+      {:inch_ex, only: :docs},
+      {:earmark, only: :docs}
+    ]
+  end
+
+  defp docs do
+    {ref, 0} = System.cmd("git", ["rev-parse", "--verify", "--quiet", "HEAD"])
+
+    [
+        source_ref: ref,
+        main: "overview"
+    ]
   end
 end
